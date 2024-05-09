4b7d36331b4663a52d3841ab726ce149caa7d9c5
Mr cute x <139148328+Mrcutex1@users.noreply.github.com>
2024-05-09 17:07:49 +0530

    mustjoin.py

--- DAXXMUSIC/plugins/bot/mustjoin.py
+++ DAXXMUSIC/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from DAXXMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "aboutcutex"
+MUST_JOIN = "Ace_networkop"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):

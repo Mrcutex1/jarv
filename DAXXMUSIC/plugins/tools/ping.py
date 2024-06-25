d97dbe284db9143b9e005abb0b3bd65f5d91f0e9
Mr cute x <139148328+Mrcutex1@users.noreply.github.com>
2024-06-25 09:11:46 +0530

    ping.py

--- DAXXMUSIC/plugins/tools/ping.py
+++ DAXXMUSIC/plugins/tools/ping.py
@@ -17,7 +17,7 @@ from config import BANNED_USERS
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_video(
-        video="https://graph.org/file/945976e593fe3889929a3.mp4",
+        video="https://graph.org/file/2aa9629758f600d009463.mp4",
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await DAXX.ping()

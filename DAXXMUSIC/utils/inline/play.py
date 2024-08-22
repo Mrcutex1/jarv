d1ffaf8f8c6902389d76a7a6adc123b1fa69dcd1
Mr cute x <139148328+Mrcutex1@users.noreply.github.com>
2024-08-22 12:41:48 +0530

    Update play.py

--- DAXXMUSIC/utils/inline/play.py
+++ DAXXMUSIC/utils/inline/play.py
@@ -67,8 +67,9 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-         InlineKeyboardButton(text="😈ᴏᴡɴᴇʀ😈", user_id=config.OWNER_ID),
-         InlineKeyboardButton(text="✨sᴜᴘᴘᴏʀᴛ✨", url=f"{SUPPORT_CHAT}",),
+        # InlineKeyboardButton(text="😈ᴏᴡɴᴇʀ😈", user_id=config.OWNER_ID),
+        # InlineKeyboardButton(text="✨sᴜᴘᴘᴏʀᴛ✨", url=f"{SUPPORT_CHAT}",),
+         InlineKeyboardButton(text="✨Mᴏᴠɪᴇs+Sᴇʀɪᴇs+Aɴɪᴍᴇ🎬", url=f"https://t.me/okflix_ottbot?start=start",),
         ],
         [InlineKeyboardButton(text="•ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ•", url=f"https://t.me/Miss_ruhi_bot?startgroup=true")],
     ]
@@ -84,8 +85,9 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-         InlineKeyboardButton(text="😈ᴏᴡɴᴇʀ😈", user_id=config.OWNER_ID),
-         InlineKeyboardButton(text="✨sᴜᴘᴘᴏʀᴛ✨", url=f"{SUPPORT_CHAT}",),
+         #InlineKeyboardButton(text="😈ᴏᴡɴᴇʀ😈", user_id=config.OWNER_ID),
+         #InlineKeyboardButton(text="✨sᴜᴘᴘᴏʀᴛ✨", url=f"{SUPPORT_CHAT}",),
+         InlineKeyboardButton(text="✨Mᴏᴠɪᴇs+Sᴇʀɪᴇs+Aɴɪᴍᴇ🎬", url=f"https://t.me/okflix_ottbot?start=start",),
         ],
         [InlineKeyboardButton(text="•ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ•", url=f"https://t.me/Miss_ruhi_bot?startgroup=true")],
     ]
